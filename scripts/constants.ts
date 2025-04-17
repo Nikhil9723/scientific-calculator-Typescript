2aad672531dc3f7e875fabccd3f53ed5de6161d2
Nikhil Prajapati <nikhil.prajapati@simformsolutions.com>
2025-04-17 11:19:16 +0530

    Added constants file for buttons value

---
+++ scripts/constants.ts
@@ -0,0 +1,43 @@
+const BUTTON_TYPES = {
+    EQUAL: "=",
+    FUNCTION_EXPONENTIAL: 'F-E',
+    DEG: "DEG",
+    RAD: "RAD",
+    CLEAR_SCREEN: "C",
+    BACKSPACE: "backspace",
+    MULTIPLICATION: "*",
+    DIVIDE: "/",
+    FACTORIAL: "factorial",
+    SQUARE: "square",
+    SECOND_FUNCTIONALITY: "2nd",
+    POWER_OF_10: "powerof10",
+    INVERSE: "1/",
+    ABSOLUTE_VALUE: "absolute-value",
+    EXPONENET: "exp",
+    POWER: "power",
+    SQUARE_ROOT: "squareRoot",
+    LOG: "log",
+    LN: "ln",
+    E: "e",
+    PLUSH_MINUS: "+/-",
+    SINEX: "sin",
+    COSEX: "cos",
+    TANEX: "tan",
+    FLOOR: "floor",
+    CEIL: "ceil",
+    MEMORY_PLUSH: "M+",
+    MEMORY_MINUS: "M-",
+    MEMORY_SAVE: "MS",
+    MEMORY_READ: "MR",
+    MEMORY_CLEAR: "MC",
+    };
+
+
+    const ELEMENT_IDS = {
+        SQUARE: "square",
+        SQUARE_ROOT: "squareRoot",
+        DEG_BTN: "deg-btn",
+        };
+
+
+export  {BUTTON_TYPES, ELEMENT_IDS}
