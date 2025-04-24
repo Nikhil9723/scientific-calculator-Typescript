e3c30fd970c22694a879b39a3e9017c662bf8a50
Nikhil Prajapati <nikhil.prajapati@simformsolutions.com>
2025-04-24 11:35:28 +0530

    Update the constant.js covert it to the enums

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,43 +1,43 @@
-const BUTTON_TYPES = {
-    EQUAL: "=",
-    FUNCTION_EXPONENTIAL: 'F-E',
-    DEG: "DEG",
-    RAD: "RAD",
-    CLEAR_SCREEN: "C",
-    BACKSPACE: "backspace",
-    MULTIPLICATION: "*",
-    DIVIDE: "/",
-    FACTORIAL: "factorial",
-    SQUARE: "square",
-    SECOND_FUNCTIONALITY: "2nd",
-    POWER_OF_10: "powerof10",
-    INVERSE: "1/",
-    ABSOLUTE_VALUE: "absolute-value",
-    EXPONENET: "exp",
-    POWER: "power",
-    SQUARE_ROOT: "squareRoot",
-    LOG: "log",
-    LN: "ln",
-    E: "e",
-    PLUSH_MINUS: "+/-",
-    SINEX: "sin",
-    COSEX: "cos",
-    TANEX: "tan",
-    FLOOR: "floor",
-    CEIL: "ceil",
-    MEMORY_PLUSH: "M+",
-    MEMORY_MINUS: "M-",
-    MEMORY_SAVE: "MS",
-    MEMORY_READ: "MR",
-    MEMORY_CLEAR: "MC",
+const enum BUTTON_TYPES  {
+    EQUAL= "=",                         // To evalute the string (result)
+    FUNCTION_EXPONENTIAL= 'F-E',        // To calculate the functional exponent F-E
+    DEG= "DEG",                         // To calculate result in deg and toogle the button of deg
+    RAD= "RAD",                         // To calculate output in RAD
+    CLEAR_SCREEN= "C",                  // Clear the screen
+    BACKSPACE= "backspace",             // remove the last character
+    MULTIPLICATION= "*",                // function for multiplication
+    DIVIDE= "/",                        // function for division of two number
+    FACTORIAL= "factorial",             // function for factorial
+    SQUARE= "square",                   // function for square
+    SECOND_FUNCTIONALITY= "2nd",        // function for second functionality of calculator
+    POWER_OF_10= "powerof10",           // function to calcualte the power of 10
+    INVERSE= "1/",                      // function for inverse the number
+    ABSOLUTE_VALUE= "absolute-value",   // function for absolute value
+    EXPONENET= "exp",                   // function for exponent
+    POWER= "power",                     // function for calculate power
+    SQUARE_ROOT= "squareRoot",          // function for squareRoot of number
+    LOG= "log",                         // function for calculate log of the number
+    LN= "ln",                           // function for ln of number
+    E= "e",                             // function for calculate the exponent of value
+    PLUSH_MINUS= "+/-",                 // function for to toogle sign of number form + to - and - to +
+    SINEX= "sin",                       // function for calculate the trignometry function sin
+    COSEX= "cos",                       // function for calculate the trignometry function cos
+    TANEX= "tan",                       // function for calculate the trignometry function tan
+    FLOOR= "floor",                     // function for calculate the floor of number
+    CEIL= "ceil",                       // function for calculate the ceil of number
+    MEMORY_PLUSH= "M+",                 // function for to add the value to the memory variable
+    MEMORY_MINUS= "M-",                 // function for divison of number
+    MEMORY_SAVE= "MS",                  // function for to save the number into memory
+    MEMORY_READ= "MR",                  // function for to read the number form mrmory
+    MEMORY_CLEAR= "MC",                 // function for to clear the memory inputs
     };
 
 
-    const ELEMENT_IDS = {
-        SQUARE: "square",
-        SQUARE_ROOT: "squareRoot",
-        DEG_BTN: "deg-btn",
-        };
+ const enum ELEMENT_IDS {
+    SQUARE= "square",                   // id to get the button for square of the number
+    SQUARE_ROOT= "squareRoot",          // id for square root of the number
+    DEG_BTN= "deg-btn",                 // id to get deg button to toogle between the deg and rad button
+};
 
 
 export  {BUTTON_TYPES, ELEMENT_IDS}
